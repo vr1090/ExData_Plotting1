302815ba4b2be25c9b320a8fd1eea498a3ec739d
Viranuvatti Adhi Praganta <v.adhi@samsung.com>
2016-04-14 19:45:36 +0700

    update loading data

--- loadData.r
+++ loadData.r
@@ -3,4 +3,6 @@ data$dateStr <- strptime(data$Date,format = "%d/%m/%Y")
 data$dateConvert <- as.Date(data$dateStr)
 selector <- with(data, dateConvert >= '2007-02-01' & dateConvert <='2007-02-02')
 dataClean <- data[selector,]
+dataClean$dateStr <- paste(  dataClean$Date,dataClean$Time)
+dataClean$dateConvert <- strptime( dataClean$dateStr,format="%d/%m/%Y %H:%M:%S")
 save(dataClean,file = "dataClean.rdata")
