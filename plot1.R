14b6b5e45044db0ae379a0e371fb3cee2cf34864
Viranuvatti Adhi Praganta <v.adhi@samsung.com>
2016-04-14 16:46:48 +0700

    add plot1 code

---
+++ plot1.R
@@ -0,0 +1,11 @@
+#loading rhe data
+filename <- "dataClean.rdata"
+load(filename)
+
+#start creating plot..
+dataGlobalActive <- as.numeric( dataClean$Global_active_power)
+data_global_kw <- dataGlobalActive/1000
+png(filename ="plot1.png" )
+hist(data_global_kw,main = "Global Active Power",col=c("red"),xlab = "Global Active Power (kilowatts)")
+dev.off()
+#end of historgram code
