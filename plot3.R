c1d124f264e27fe84042518ed0fc6c1e1c12c517
inu pragantha <inu.pragantha@gmail.com>
2016-04-16 00:22:10 +0700

    update lagi

--- plot3.R
+++ plot3.R
@@ -7,6 +7,6 @@ png(filename="plot3.png")
 with(dataClean, plot(dateConvert,Sub_metering_1, type="S", ylab="Energy sub metering",xlab=""))
 with(dataClean, points(dateConvert,Sub_metering_2, col="red", type="S"))
 with(dataClean, points(dateConvert,Sub_metering_3, col="blue", type="S"))
-legend("topright",col=c("black","red","blue"),pch="----",legend=c("energy sub metering 1","energy sub metering 2","energy sub metering 3"))
+legend("topright",col=c("black","red","blue"),lty=1,lwd=3,legend=c("energy sub metering 1","energy sub metering 2","energy sub metering 3"))
 
 dev.off()
